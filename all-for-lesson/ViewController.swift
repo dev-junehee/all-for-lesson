be5bfbb3dfa0078dcff44bda74bcd9e91eb5a8e7
dev-junehee <dev.junehee@gmail.com>
2024-08-15 15:18:19 +0900

    Initial Commit

---
+++ all-for-lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  all-for-lesson
+//
+//  Created by junehee on 8/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
