8e56c77f44340bf324431dc5f9037233b3a7eba1
dev-junehee <dev.junehee@gmail.com>
2024-12-07 23:46:52 +0900

    Feat: NetworkInterceptor 구현, 토큰 갱신 로직 수정

--- all-for-lesson/Manager/NetworkManager.swift
+++ all-for-lesson/Manager/NetworkManager.swift
@@ -14,55 +14,83 @@ final class NetworkManager {
     static let shared = NetworkManager()
     private init() { }
     
+    /// 재귀 함수를 활용한 토큰 갱신 포함
+    // func apiCall<T: Decodable>(api: APIRouter, of type: T.Type) -> Single<Result<T, NetworkErrorCase>> {
+    //     return Single<Result<T, NetworkErrorCase>>.create { observer -> Disposable in
+    //         do {
+    //             var request = try api.asURLRequest()
+    //             
+    //             func callRequest(isRefresh: Bool = false) {
+    //                 /// 토큰 리프레시 후 다시 API Call 시도할 때 request도 갱신 해주기
+    //                 if isRefresh {
+    //                     do {
+    //                         request = try api.asURLRequest()
+    //                     } catch {
+    //                         print("API URLRequestConvertible Failed - Refresh", error)
+    //                     }
+    //                 }
+    //                 AF.request(request)
+    //                     .validate(statusCode: 200..<300)
+    //                     .responseDecodable(of: T.self) { response in
+    //                         guard let statusCode = response.response?.statusCode else { return }
+    //                         print("상태코드 확인 >>>", statusCode)
+    //                         if statusCode == 419 {
+    //                             print("액세스 토큰 갱신 필요")
+    //                             self.refreshToken { success in /// 토큰 갱신에 성공하면 네트워크 재호출, 실패하면 에러 처리
+    //                                 print("토큰 갱신 결과", success)
+    //                                 if success {
+    //                                     callRequest(isRefresh: true)
+    //                                     return
+    //                                 } else {
+    //                                     guard let networkError = NetworkErrorCase(rawValue: statusCode) else {
+    //                                         return observer(.success(.failure(NetworkErrorCase.UnknownError)))
+    //                                     }
+    //                                     observer(.success(.failure(networkError)))
+    //                                 }
+    //                             }
+    //                             return
+    //                         }
+    //                         switch response.result {
+    //                         case .success(let value):
+    //                             observer(.success(.success(value)))
+    //                         case .failure(_):
+    //                             guard let networkError = NetworkErrorCase(rawValue: statusCode) else {
+    //                                 return observer(.success(.failure(NetworkErrorCase.UnknownError)))
+    //                             }
+    //                             observer(.success(.failure(networkError)))
+    //                         }
+    //                     }
+    //             }
+    //             
+    //             callRequest()
+    //             return Disposables.create()
+    //         } catch {
+    //             print("API URLRequestConvertible Failed", error)
+    //             return Disposables.create()
+    //         }
+    //     }.debug("API 네트워크 통신 >>>")
+    // }
+    
+    /// 인터셉터를 활용한 토큰 갱신 포함
     func apiCall<T: Decodable>(api: APIRouter, of type: T.Type) -> Single<Result<T, NetworkErrorCase>> {
         return Single<Result<T, NetworkErrorCase>>.create { observer -> Disposable in
             do {
-                var request = try api.asURLRequest()
-                
-                func callRequest(isRefresh: Bool = false) {
-                    /// 토큰 리프레시 후 다시 API Call 시도할 때 request도 갱신 해주기
-                    if isRefresh {
-                        do {
-                            request = try api.asURLRequest()
-                        } catch {
-                            print("API URLRequestConvertible Failed - Refresh", error)
+                let request = try api.asURLRequest()
+                AF.request(request, interceptor: NetworkInterceptor())
+                    .validate(statusCode: 200..<300)
+                    .responseDecodable(of: T.self) { response in
+                        guard let statusCode = response.response?.statusCode else { return }
+                        print("상태코드 확인 >>>", statusCode)
+                        switch response.result {
+                        case .success(let value):
+                            observer(.success(.success(value)))
+                        case .failure(_):
+                            guard let networkError = NetworkErrorCase(rawValue: statusCode) else {
+                                return observer(.success(.failure(NetworkErrorCase.UnknownError)))
+                            }
+                            observer(.success(.failure(networkError)))
                         }
                     }
-                    print("URL >>>", request.url)
-                    AF.request(request)
-                        .validate(statusCode: 200..<300)
-                        .responseDecodable(of: T.self) { response in
-                            guard let statusCode = response.response?.statusCode else { return }
-                            print("상태코드 확인 >>>", statusCode)
-                            if statusCode == 419 {
-                                print("액세스 토큰 갱신 필요")
-                                self.refreshToken { success in /// 토큰 갱신에 성공하면 네트워크 재호출, 실패하면 에러 처리
-                                    print("토큰 갱신 결과", success)
-                                    if success {
-                                        callRequest(isRefresh: true)
-                                        return
-                                    } else {
-                                        guard let networkError = NetworkErrorCase(rawValue: statusCode) else {
-                                            return observer(.success(.failure(NetworkErrorCase.UnknownError)))
-                                        }
-                                        observer(.success(.failure(networkError)))
-                                    }
-                                }
-                                return
-                            }
-                            switch response.result {
-                            case .success(let value):
-                                observer(.success(.success(value)))
-                            case .failure(_):
-                                guard let networkError = NetworkErrorCase(rawValue: statusCode) else {
-                                    return observer(.success(.failure(NetworkErrorCase.UnknownError)))
-                                }
-                                observer(.success(.failure(networkError)))
-                            }
-                        }
-                }
-                
-                callRequest()
                 return Disposables.create()
             } catch {
                 print("API URLRequestConvertible Failed", error)
@@ -106,27 +134,6 @@ final class NetworkManager {
             return Disposables.create()
         }.debug("Post Files 네트워크 통신 >>>")
     }
-    
-    // func postReservation(id: String, reservation: Bool) {
-    //     let URL = API.URL.posts + id + API.URL.bookmark
-    //     let headers: HTTPHeaders = [
-    //         API.Header.auth: UserDefaultsManager.accessToken,
-    //         API.Header.sesacKey: API.KEY.key
-    //     ]
-    //     let params: [String: Bool] = [
-    //         "like_status": reservation
-    //     ]
-    //     
-    //     AF.request(URL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers)
-    //         .responseDecodable(of: ReservationResponse.self) { result in
-    //             switch result.result {
-    //             case .success(let value):
-    //                 print(value)
-    //             case .failure(let error):
-    //                 print(error)
-    //             }
-    //         }
-    // }
     
     func getImage(_ file: String, completion: @escaping (Data) -> Void) {
         let URL = API.URL.base + file
@@ -198,6 +205,71 @@ final class NetworkManager {
             print("Refresh URLRequestConvertible Failed")
         }
     }
-        
+    
+    func refreshTokenWithInterceptor() -> Single<(statuscode: Int, data: RefreshTokenResponse?)> {
+        do {
+            let request = try UserRouter.accessToken.asURLRequest()
+            return Single.create { observer in
+                let disposable = AF.request(request, interceptor: NetworkInterceptor())
+                    .validate(statusCode: 200..<300)
+                    .responseDecodable(of: RefreshTokenResponse.self) { response in
+                        guard let statusCode = response.response?.statusCode else {
+                            observer(.success((statuscode: 0, data: nil)))
+                            return
+                        }
+                        switch response.result {
+                        case .success(let value):
+                            observer(.success((statuscode: statusCode, data: value)))
+                        case .failure:
+                            observer(.success((statuscode: statusCode, data: nil)))
+                        }
+                    }
+                return Disposables.create {
+                    disposable.cancel()
+                }
+            }
+        } catch {
+            print("Refresh URLReqeustConvertible Failed")
+            return Single.just((statuscode: 0, data: nil))
+        }
+    }
+
+    
 }
 
+final class NetworkInterceptor: RequestInterceptor {
+    
+    private let disposeBag = DisposeBag()
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, any Error>) -> Void) {
+        print("Interceptor ADAPT Start")
+        var request = urlRequest
+        request.setValue(UserDefaultsManager.accessToken, forHTTPHeaderField: API.Header.auth)
+        print(request.headers)
+        completion(.success(request))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: any Error, completion: @escaping (RetryResult) -> Void) {
+        print("Interceptor RE-TRY Start")
+        
+        guard let response = request.task?.response as? HTTPURLResponse,
+              response.statusCode == 419 else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        NetworkManager.shared.refreshTokenWithInterceptor()
+            .subscribe { result in
+                guard let token = result.data?.accessToken else {
+                    completion(.doNotRetry)
+                    return
+                }
+                UserDefaultsManager.accessToken = token
+                completion(.retry)
+            } onFailure: { error in
+                print(error)
+                completion(.doNotRetry)
+            }
+            .disposed(by: disposeBag)
+    }
+}
